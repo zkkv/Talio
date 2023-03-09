449222497f1301a01c6c74b3a4c14ac257b66b23
Aleksandar Yotkov <a.p.yotkov-1@student.tudelft.nl>
2023-03-09 19:40:32 +0100

    Add annotation

--- server/src/main/java/server/database/BoardRepository.java
+++ server/src/main/java/server/database/BoardRepository.java
@@ -2,6 +2,9 @@ package server.database;
 
 import commons.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
 }
--- server/src/main/java/server/database/CardListRepository.java
+++ server/src/main/java/server/database/CardListRepository.java
@@ -2,5 +2,7 @@ package server.database;
 
 import commons.CardList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CardListRepository extends JpaRepository<CardList, Long> {}
