a7af8598768ef5b200f1721b47b6f0f16748c67b
Aleksandar Yotkov <a.p.yotkov-1@student.tudelft.nl>
2023-03-09 19:57:14 +0100

    Extract method that draws the list and making a new one to draw and make a request

--- client/src/main/java/client/scenes/HomeScreenCtrl.java
+++ client/src/main/java/client/scenes/HomeScreenCtrl.java
@@ -2,15 +2,27 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import commons.CardList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import java.awt.*;
+import javafx.scene.text.Font;
+import javafx.scene.control.Label;
+
+import javafx.scene.control.Button;
+import javafx.scene.text.TextAlignment;
+
+import java.util.ArrayList;
+
 
 public class HomeScreenCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+
     @FXML
     private HBox panel;
     @FXML
@@ -21,7 +33,13 @@ public class HomeScreenCtrl {
         this.mainCtrl = mainCtrl;
         this.server = server;
     }
-    public void createList(){
+
+    public void createList() {
+        drawCardList("Label");
+        server.addCardListToBoard(new CardList(new ArrayList<>(), "Label"));
+    }
+
+    public void drawCardList(String text) {
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
         vbox.setLayoutX(21.0);
@@ -29,7 +47,32 @@ public class HomeScreenCtrl {
         vbox.setPrefHeight(36.0);
         vbox.setPrefWidth(126.0);
         vbox.setStyle("-fx-background-color: #d9cdad; -fx-border-color: black;");
+        HBox hbox = new HBox();
+        Region region = new Region();
+        Insets insets = new Insets(0, 20.0, 0, 0);
+        region.setLayoutX(63.0);
+        region.setLayoutY(10.0);
+        region.setPadding(insets);
+        Label label = new Label();
+        Font font = new Font(16.0);
+        label.setFont(font);
+        label.setText(text);
+        label.setAlignment(Pos.TOP_CENTER);
+        Region region2 = new Region();
+        Insets insets2 = new Insets(0, 5.0, 0, 0);
+        region2.setPadding(insets2);
+        Button button = new Button(":");
+        button.setAlignment(Pos.TOP_CENTER);
+        button.setTextAlignment(TextAlignment.CENTER);
+        button.setMnemonicParsing(false);
+        button.setStyle("-fx-background-color: #a3957c;");
+        Font font2 = new Font("Arial Nova Cond", 13.0);
+        button.setFont(font2);
+        hbox.getChildren().add(region);
+        hbox.getChildren().add(label);
+        hbox.getChildren().add(button);
+        hbox.getChildren().add(region2);
+        vbox.getChildren().add(hbox);
         panel.getChildren().add(vbox);
     }
-
 }
