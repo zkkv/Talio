f7da4df0cbb234c07a909ef05ade1e91efbaf385
Sofia Dimieva <S.Dimieva@student.tudelft.nl>
2023-03-09 19:47:20 +0100

    front-end design of board with lists

--- client/src/main/java/client/scenes/HomeScreenCtrl.java
+++ client/src/main/java/client/scenes/HomeScreenCtrl.java
@@ -4,17 +4,21 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import java.awt.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+
 
 public class HomeScreenCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
     @FXML
     private HBox panel;
+
     @FXML
-    private ScrollPane scroll;
+    private AnchorPane anchorPane;
 
     @Inject
     public HomeScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
@@ -23,15 +27,36 @@ public class HomeScreenCtrl {
 
     }
     public void createList(){
+        BorderPane bp = new BorderPane();
+        bp.setPrefHeight(274);
+        bp.setPrefWidth(126);
+        bp.setStyle("-fx-background-color: #d9cdad; -fx-border-color: black;");
+        Button button = new Button(":");
+        button.setAlignment(Pos.TOP_CENTER);
+        button.setLayoutX(95.0);
+        button.setLayoutY(6.0);
+        button.setTextAlignment(TextAlignment.CENTER);
+        button.setMnemonicParsing(false);
+        button.setStyle("-fx-background-color: #a3957c;");
+        Label label = new Label();
+        label.setAlignment(Pos.TOP_CENTER);
+        label.setLayoutX(12.0);
+        label.setLayoutY(9.0);
+        label.setPrefHeight(18.0);
+        label.setPrefWidth(95.0);
+        label.setText("Name");
+        Font font = new Font(16.0);
+        label.setFont(font);
+        AnchorPane ap = new AnchorPane();
+        ap.setPrefHeight(34.0);
+        ap.getChildren().add(label);
+        ap.getChildren().add(button);
+        bp.setTop(ap);
         VBox vbox = new VBox();
+        vbox.setPrefHeight(221.0);
+        vbox.setPrefWidth(109.0);
         vbox.setAlignment(Pos.CENTER);
-        vbox.setLayoutX(21.0);
-        vbox.setLayoutY(93.0);
-        vbox.setPrefHeight(36.0);
-        vbox.setPrefWidth(126.0);
-        vbox.setStyle("-fx-background-color: #d9cdad; -fx-border-color: black;");
-        panel.getChildren().add(vbox);
+        bp.setCenter(vbox);
+        panel.getChildren().add(bp);
     }
-
-
 }
