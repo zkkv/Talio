babf9b43d095015684eb8f2dea3b27578816a23a
Kirill Zhankov <k.zhankov@student.tudelft.nl>
2023-03-09 16:20:22 +0100

    Add pop-up window when server ip is wrong

--- client/src/main/java/client/scenes/ClientConnectCtrl.java
+++ client/src/main/java/client/scenes/ClientConnectCtrl.java
@@ -4,7 +4,10 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class ClientConnectCtrl {
@@ -25,9 +28,16 @@ public class ClientConnectCtrl {
             ServerUtils.setServer(ipAddress.getText());
             ServerUtils.testConnection();
             mainCtrl.showStartPage();
-        }
-        catch (IOException e) {
-            System.out.println("Wrong ip");
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Connection Error");
+            alert.setHeaderText("Connection Error");
+            alert.setContentText("The application could not connect to the server. " +
+                    "Check the correctness of the IP-address and try again.");
+
+            Stage currentStage = (Stage) ipAddress.getScene().getWindow();
+            alert.initOwner(currentStage);
+            alert.showAndWait();
         }
 
     }
