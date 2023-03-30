1242868b01e8bfe771466576d21925390ae9ea95
Sofia Dimieva <S.Dimieva@student.tudelft.nl>
2023-03-30 15:23:18 +0200

    Setting a scene to remove and delete a board

---
+++ client/src/main/java/client/scenes/BoardSettingsCtrl.java
@@ -0,0 +1,46 @@
+package client.scenes;
+
+import client.services.BoardIdentifier;
+import client.services.BoardOverviewService;
+import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+public class BoardSettingsCtrl {
+
+    private final MainCtrl mainCtrl;
+
+    private final BoardIdentifier boardIdentifier;
+    private final BoardOverviewService boardOverviewService;
+
+    @FXML
+    private TextField boardTitle;
+
+
+    @Inject
+    public BoardSettingsCtrl(MainCtrl mainCtrl, BoardIdentifier boardIdentifier,
+                             BoardOverviewService boardOverviewService) {
+        this.mainCtrl = mainCtrl;
+        this.boardIdentifier = boardIdentifier;
+        this.boardOverviewService = boardOverviewService;
+    }
+    public void editBoardName() {
+        boardIdentifier.setCurrentBoard(boardOverviewService.updateBoardTitle(
+                boardIdentifier.getCurrentBoard(),
+                boardTitle.getText()));
+        mainCtrl.showBoardPage();
+    }
+
+    public void removeBoard() {
+        boardOverviewService.removeBoard(boardIdentifier.getCurrentBoard());
+        mainCtrl.showStartPage();
+    }
+
+    public void goBack(){
+        mainCtrl.showBoardPage();
+    }
+
+}
