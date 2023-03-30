560cfc6abba23841a6e7ebfa8221ebe1bc1abfca
Diana Şutac <d.o.sutac@student.tudelft.nl>
2023-03-30 19:41:33 +0300

    Add class to generate a random key for each board

---
+++ server/src/main/java/server/generators/SequenceGenerator.java
@@ -0,0 +1,18 @@
+package server.generators;
+
+import java.util.Random;
+
+public class SequenceGenerator {
+    public static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public String generate() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int index = random.nextInt(chars.length());
+            char randomChar = chars.charAt(index);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
+}
