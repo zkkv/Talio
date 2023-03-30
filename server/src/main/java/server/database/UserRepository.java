0c0b3306d47b3adf3ddfad61e369f4584120938f
Diana Şutac <d.o.sutac@student.tudelft.nl>
2023-03-30 19:39:15 +0300

    Add user repository

---
+++ server/src/main/java/server/database/UserRepository.java
@@ -0,0 +1,8 @@
+package server.database;
+
+import commons.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findUserByUserName(String userName);
+}
